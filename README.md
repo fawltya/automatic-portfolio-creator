990c6583a53ec7a106c011513c9df2077ed2658d
Seamus Barnett <seamus0689@gmail.com>
2023-02-17 00:30:48 +0000

    inital commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Automatic Portfolio Creator
+
+## Description
+This application lets you fill out a short quiz and uses the answers to populate content in a fresh index.html webpage. Comes with pre-done styling and node.js.
+
+![](./images/quiz-screenshot.png)
+
+![](./images/portfolio-webpage.png)
+
+## Usage
+Download the package.
+
+Open up terminal, navigate to where the downloaded folder is and type the following command:
+```javascript
+node index.js
+```
+A new index.html file will be created now. Try opening it in a browser to see the results!
+
+## License 
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ index.js
@@ -0,0 +1,85 @@
+// const inquirer = require("inquirer");
+import inquirer from "inquirer";
+import fs from "fs";
+
+inquirer
+  .prompt([
+    {
+      name: "user_name",
+      message: "What is your name?",
+    },
+    {
+      name: "location",
+      message: "Where do you live?",
+    },
+    {
+      name: "email",
+      message: "What is your email?",
+    },
+    {
+      name: "bio",
+      message: "Tell me a little about yourself.",
+    },
+    {
+      name: "linkedin",
+      message: "What is your LinkedIn URL?",
+    },
+    {
+      name: "github",
+      message: "What is your GitHub URL?",
+    },
+  ])
+
+  .then((answer) => {
+    // console.log("hello " + answer.user_name);
+    fs.writeFile(
+      "./index.html",
+      `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link href="./src/styles.css" rel="stylesheet" />
+      
+          <title>Portfolio Prompt</title>
+        </head>
+        <body>
+          <div class="main-container">
+            <div>
+              <h1>${answer.user_name}'s Portfolio</h1>
+            </div>
+            <div class="main-details">
+              <h2>Hi, my name is ${answer.user_name}. Welcome to my portfolio</h2>
+              <div class="horizontal-div">
+                <div class="location">I'm currently based in ${answer.location}</div>
+                <div class="contact">Feel free to <a href="mailto:${answer.email}" target="_blank">contact me</a></div>
+              </div>
+              <p class="bio">${answer.bio}</p>
+              <div class="horizontal-div">
+                <a href="${answer.linkedin}" target="_blank"
+                  ><img src="./images/linkedin.svg" class="linkedin-icon icon"
+                /></a>
+                <a href="mailto:${answer.email}" target="_blank"
+                  ><img src="./images/envelope-solid.svg" class="email-icon icon"
+                /></a>
+                <a href="${answer.github}" target="_blank"
+                  ><img src="./images/github.svg" class="github-icon icon"
+                /></a>
+              </div>
+            </div>
+          </div>
+          <script type="text/javascript" src="index.js"></script>
+        </body>
+      </html>
+      
+    `,
+      function (err) {
+        if (err) {
+          return console.log(err);
+        }
+        console.log("The file was saved!");
+      }
+    );
+  });
